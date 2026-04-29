0f3eef9d45e51499cb808d6939306e779189ff8b
Shraddha Akolkar <shraddhaaakolkar@gmail.com>
2026-04-29 22:49:57 +0530

    all final  done filteration start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Login from "./admin/login/Login";
 import Dashboard from "./admin/dashboard/Dashboard";
 import Doctors from "./admin/doctors/Doctors";
 import Home from "./Pages/Home";
 import AdminLayout from "./admin/layout/AdminLayout";
 import CertainCard from "./components/CertainCard/CertainCard";
+
+function ProtectedAdminRoute() {
+  const admin = localStorage.getItem("admin");
+  return admin ? <Outlet /> : <Navigate to="/admin" replace />;
+}
 
 function App() {
   return (
@@ -13,10 +18,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Login />} />
 
-<Route path="/certaincard/:id" element={<CertainCard />} />
-        <Route element={<AdminLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/doctors" element={<Doctors />} />
+        <Route path="/certaincard/:id" element={<CertainCard />} />
+        <Route element={<ProtectedAdminRoute />}>
+          <Route element={<AdminLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/doctors" element={<Doctors />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
