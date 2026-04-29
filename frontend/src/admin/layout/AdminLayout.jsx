a8b720aa8418386e136a4aa60f42aa56b22573ca
Shraddha Akolkar <shraddhaaakolkar@gmail.com>
2026-04-29 22:50:07 +0530

    -

--- frontend/src/admin/layout/AdminLayout.jsx
+++ frontend/src/admin/layout/AdminLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
-// import "./Layout.css";
 
 function AdminLayout() {
   const navigate = useNavigate();
